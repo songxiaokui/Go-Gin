496f0f34fb1646e27ea25289640429d948cecff7
宋晓奎 <austsxk@163.com>
2021-04-01 10:59:14 +0800

    panic错误统一使用自定义响应错误处理

--- Proxies/internal/proxy/pkg/middleware/error.go
+++ Proxies/internal/proxy/pkg/middleware/error.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	myrsp "Go-Gin/Proxies/internal/proxy/pkg/responseJson"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 /*
@@ -19,7 +21,12 @@ func ErrorMiddleware() gin.HandlerFunc {
 		// 捕获异常
 		defer func() {
 			if err := recover(); err != nil {
-				context.JSON(http.StatusBadRequest, gin.H{"msg": err})
+				// 修改使用封装好的包
+				msg := "请求错误"
+				if m, ok := err.(string); ok {
+					msg = m
+				}
+				myrsp.R(context)(strconv.Itoa(http.StatusBadRequest), msg, nil)(myrsp.Failed)
 			}
 		}()
 		context.Next()
